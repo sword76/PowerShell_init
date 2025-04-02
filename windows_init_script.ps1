e95716cb36647a33b73bc59d3fd85578e90bb99d
Sergey Chirva <sword76@users.noreply.github.com>
2025-04-02 11:25:02 +0300

    Update windows_init_script.ps1
    Added Python installation.

--- windows_init_script.ps1
+++ windows_init_script.ps1
@@ -20,27 +20,29 @@ $ifIndex = $ipInterfaces | Where-Object { $_.ifAlias -eq "Беспроводно
 <#
 Установить приложения по умолчанию
 #>
-winget install --id Git.Git -e --source winget;   # GitHub Desktop
+winget install --id Git.Git -e --source winget;   # GitHub Desktop
 if ($?) { winget install --id Microsoft.VisualStudioCode -e --source winget --scope user };
+if ($?) { winget install --id Python.Python.3.13 -e --Source --source winget };
+if ($?) { winget install --id Adobe.Acrobat.Reader.64-bit -e --source winget };
 if ($?) { winget install --id qBittorrent.qBittorrent -e --source winget };
 if ($?) { winget install --id Google.Chrome -e --source winget };
 if ($?) { winget install --id 7zip.7zip -e --source winget };
 if ($?) { winget install --id DEVCOM.JetBrainsMonoNerdFont -e --source winget };
 if ($?) { winget install --id Telegram.TelegramDesktop -e --source winget --scope user };
-# if ($?) { winget install --id 9NKSQGP7F2NH -e --source msstore --scope user }; # Install WhatsApp
-if ($?) { winget install --id OliverSchwendener.ueli -e --source winget --scope user }; # Ueli is a cross-platform keystroke launcher
+if ($?) { winget install --id FarManager.FarManager -e --source winget };
+
+if ($?) { winget install --id OliverSchwendener.ueli -e --source winget }; # Ueli is a cross-platform keystroke launcher
 if ($?) { winget install --id 9NLXL1B6J7LW -e --source msstore --scope user }; # Install SafeInCloud
 if ($?) { winget install --id IrfanSkiljan.IrfanView -e --source winget --scope user };
-if ($?) { winget install --id PuTTY.PuTTY -e --source winget }
+if ($?) { winget install --id LiteratureandLatte.Scrivener -e --source winget --scope user };
+
+# if ($?) { winget install --id 9NKSQGP7F2NH -e --source msstore --scope user }; # Install WhatsApp
 # if ($?) { winget install --id Mozilla.Thunderbird -e --source winget };
 # if ($?) { winget install --id Obsidian.Obsidian -e --source winget --scope user };
 # if ($?) { winget install --id Yandex.Browser -e --source winget --scope user };
 # if ($?) { winget install --id gerardog.gsudo -e --source winget }; # sudo for Powershell
 # if ($?) { winget install --id ALCPU.CoreTemp --source winget }; # Program to monitor processor temperature and other vital information
 # if ($?) { winget install --id Bitwarden.Bitwarden -e --source winget --scope user };
-
-# Установка VS Code Extensions
-
 
 # Установить WSL и включить Hyper-V
 wsl --install
